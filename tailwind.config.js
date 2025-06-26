cc20e9ef328a3b22e3a225283db3f8cd62efa5b1
MD. ALAMIN <alamin833@bb.org.bd>
2025-06-26 17:54:01 +0600

    header part complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // adjust according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+  
